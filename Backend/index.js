4165bcfe70521a954c09e8c95a555a4bc3077fde
tsugaide <arjunazarqa62@gmail.com>
2025-11-11 19:59:44 +0700

    dotenv

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import imagesRouter from "./routes/imagesRoutes.js";
 
+dotenv.config();
 const app = express();
 app.use(cors());
 
